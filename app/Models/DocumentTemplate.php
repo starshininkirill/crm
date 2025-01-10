20e0be9498311980fa529fca15febfd36ea28799
Programmer <starshinin-kirill@rambler.ru>
2025-01-10 10:52:56 +0300

    Создал сущность ШаблонДокумента. Буду подключать Inertia

---
+++ app/Models/DocumentTemplate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DocumentTemplate extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'type', 'file'];
+    public $timestamps = false;
+
+    
+}
