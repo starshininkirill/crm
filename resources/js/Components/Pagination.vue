2df961c1235e79270edc0dbf71071aa010b8db4b
Programmer <starshinin-kirill@rambler.ru>
2025-06-30 12:12:34 +0300

    фикс пагинации

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -6,7 +6,7 @@
                     class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded" v-html="link.label" />
                 <Link v-else
                     class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded focus:border-indigo-500 focus:text-indigo-500"
-                    :class="{ 'bg-white': link.active }" :href="link.url" v-html="link.label" />
+                    :class="{ 'bg-indigo-500 text-white': link.active }" :href="link.url" v-html="link.label" />
             </template>
         </div>
     </div>
